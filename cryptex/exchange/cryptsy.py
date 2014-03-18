b5aadf39ec9a876321f953cf2b4dca6a0b610b70
Chris LaRose <cjlarose@gmail.com>
2014-03-18 00:07:51 -0700

    Removed all references to fix_json_types in Crytpy
    Conflicts:
    	cryptex/exchange/cryptsy.py

--- cryptex/exchange/cryptsy.py
+++ cryptex/exchange/cryptsy.py
@@ -42,38 +42,6 @@ class CryptsyBase(object):
         aware_time = cryptsy_time.normalize(cryptsy_time.localize(naive_time)).astimezone(pytz.utc)
         return aware_time
 
-    @staticmethod
-    def fix_json_types(node, parent=None):
-        '''
-        As Crypts provides every (?) json value as unicode string,
-        this takes a json dict and converts all numeric values to Decimal
-        if their key does not end with '_id'
-        '''
-        if isinstance(node, dict):
-            for key, item in node.items():
-                node[key] = CryptsyBase.fix_json_types(item, key)
-            return node
-        elif isinstance(node, list):
-            for index, item in enumerate(node):
-                node[index] = CryptsyBase.fix_json_types(item, node)
-            return node
-        elif isinstance(node, unicode):
-            if isinstance(parent, unicode) and parent.endswith('_id'):
-                #FIXME: Does not match on trade_id
-                # If key containd _id one would not expect to find a Decimal() instance as value
-                node = int(node)
-            elif isinstance(parent, unicode) and parent.endswith('_code'):
-                # There are currency codes that are Decimal parseable
-                return node
-            else:
-                try:
-                    node = Decimal(node)
-                except InvalidOperation:
-                    pass
-            return node
-        else:
-            return node
-
 class CryptsyPublic(CryptsyBase, SingleEndpoint):
     API_ENDPOINT = 'http://pubapi.cryptsy.com/api.php'
 
@@ -81,7 +49,7 @@ class CryptsyPublic(CryptsyBase, SingleEndpoint):
         self._init_http_session()
 
     def perform_get_request(self, method='', params={}):
-        return self.fix_json_types(super(CryptsyPublic, self).perform_get_request(method, params))
+        return super(CryptsyPublic, self).perform_get_request(method, params)
 
     def get_market_data(self, market_id=None):
         '''
@@ -126,7 +94,7 @@ class Cryptsy(CryptsyBase, Exchange, SignedSingleEndpoint):
         self.timezone = None
 
     def perform_request(self, method, data={}):
-        return self.fix_json_types(super(Cryptsy, self).perform_request(method, data))
+        return super(Cryptsy, self).perform_request(method, data)
 
     def _convert_datetime(self, time_str):
         """
