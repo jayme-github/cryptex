ac61e42fa2cf89d4f471a5631624ea52e2e1011b
jayme-github <tuxnet@gmail.com>
2014-04-02 15:18:59 +0200

    Use default context to fix rounding

--- cryptex/common.py
+++ cryptex/common.py
@@ -1,10 +1,5 @@
 import decimal
 
-decimal.DefaultContext.rounding = decimal.ROUND_DOWN
-decimal.DefaultContext.traps = decimal.ExtendedContext.traps.copy()
-decimal.DefaultContext.traps[decimal.InvalidOperation] = 1
-decimal.setcontext(decimal.DefaultContext)
-
 DECIMAL_PRECISION = decimal.Decimal(10) ** -8
 
 def quantize(decimal_value):
