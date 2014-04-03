a632a52ec079c89ba56dfdd9751ade18e9457ba9
jayme-github <tuxnet@gmail.com>
2014-04-03 12:54:18 +0200

    Parse trade history from TransHistory method as it provides more accurate values

--- cryptex/exchange/btce.py
+++ cryptex/exchange/btce.py
@@ -1,5 +1,7 @@
 import datetime
 import pytz
+import re
+from decimal import Decimal
 
 from cryptex.exchange import Exchange
 from cryptex.trade import Sell, Buy
@@ -7,6 +9,7 @@ from cryptex.order import SellOrder, BuyOrder
 from cryptex.transaction import Transaction, Deposit, Withdrawal
 from cryptex.exchange.single_endpoint import SingleEndpoint, SignedSingleEndpoint
 from cryptex.exception import APIException
+import cryptex.common as common
 
 class BTCEBase(object):
     @staticmethod
@@ -103,6 +106,28 @@ class BTCE(BTCEBase, Exchange, SignedSingleEndpoint):
         self.secret = secret
         self.public = BTCEPublic()
 
+        # Set up regular expressions for transaction parsing
+        r_float = r'(?:\d+)\.?(?:\d+)?'
+        r_currency_code = r'[A-Z\d]+'
+        r_order = r'(:order:(?P<order_id>\d+):)'
+        r_fee = r'(?:\(-(?P<fee_percent>{0})%\))'.format(r_float)
+        r_amount_pair = r'(?P<amount>{0})\s+(?P<base_currency>{1})'.format(r_float,
+                                                                            r_currency_code)
+        r_price_pair = r'(?P<price>{0})\s+(?P<counter_currency>{1})'.format(r_float,
+                                                                            r_currency_code)
+        r_buy = r'{0}\s+{1}.*?{2}.*?{3}'.format(r_amount_pair,
+                                                r_fee,
+                                                r_order,
+                                                r_price_pair)
+        r_sell = r'{0}.*?{2}.*?{3}.*?(?P<total>{4})\s+(?:{5})\s+{1}'.format(r_amount_pair,
+                                                                            r_fee,
+                                                                            r_order,
+                                                                            r_price_pair,
+                                                                            r_float,
+                                                                            r_currency_code)
+        self.re_buy = re.compile(r_buy)
+        self.re_sell = re.compile(r_sell)
+
     def perform_request(self, method, data={}):
         try:
             return super(BTCE, self).perform_request(method, data)
@@ -111,27 +136,9 @@ class BTCE(BTCEBase, Exchange, SignedSingleEndpoint):
                 return {}
             else:
                 raise e
-    @staticmethod
-    def _format_trade(trade_id, trade):
-        base, counter = BTCE._pair_to_market(trade['pair'])
-        if trade['type'] == 'buy':
-            trade_type = Buy
-        else:
-            trade_type = Sell
-
-        return trade_type(
-            trade_id = trade_id,
-            base_currency = base.upper(),
-            counter_currency = counter.upper(),
-            datetime = BTCE._format_timestamp(trade['timestamp']),
-            order_id = trade['order_id'],
-            amount = trade['amount'],
-            price = trade['rate'],
-        )
 
     def get_my_trades(self):
-        trades = self.perform_request('TradeHistory')
-        return [BTCE._format_trade(t_id, t) for t_id, t in trades.iteritems()]
+        return self._parse_transaction_history(parse_transactions=False)[1]
 
     @staticmethod
     def _format_order(order_id, order):
@@ -182,32 +189,140 @@ class BTCE(BTCEBase, Exchange, SignedSingleEndpoint):
         response = self._create_order(market, 'sell', quantity, price)
         return response['order_id']
 
-    def get_my_transactions(self, limit=1000):
+    def _parse_transaction_history(self, parse_transactions=True, parse_trades=True):
+        '''
+        Parse the transaction history ("TransHistory" API method) for
+        transactions and/or trades
+
+        :param parse_transactions: parse transactions
+        :param parse_trades: parse trades
+        :rtype: tuple of lists (transactions, trades)
+        '''
+
+
+        '''
+        BTC-e types:
+            1: deposit
+            2: withdrawal
+            3: not existent?
+            4: buy and sell
+            5: buy and sell
+        '''
+
+        if parse_trades:
+            trade_history = self.perform_request('TradeHistory', {'count': 9999999999999})
+
+        def get_trade_id(t, m):
+            '''
+            Get the trade_id as it is not provided via TransHistory method
+            '''
+            trades = filter(lambda v: int(v[1]['timestamp']) == int(t.get('timestamp')) and \
+                            int(v[1]['order_id']) == int(m.groupdict().get('order_id')),
+                            trade_history.iteritems())
+
+            if len(trades) == 1:
+                return int(trades[0][0])
+            elif len(trades) > 1:
+                # still more than one possible trade_id
+                # use the id of the trade with the "most equal" amount
+                # as the amount from TradeHistory may be rounded
+                best = [None, None]
+                for tid, trade in trades:
+                    lookin = m.groupdict().get('amount')
+                    lookfor = str(trade['amount'])
+                    if lookfor == lookin:
+                        return tid
+
+                    score = 0
+                    # Iterate over amount to see how many digits match
+                    for idx in range(len(lookin)):
+                        try:
+                            if lookin[idx] != lookfor[idx]:
+                                score = idx
+                                break
+                        except IndexError:
+                            score = idx - 1
+                            break
+
+                    if best[0] is None or score > best[0]:
+                        # This amount mathes better than the last one
+                        best[0] = score
+                        best[1] = tid
+                if best[1] is not None:
+                    return best[1]
+
+            raise ValueError('No trade_id found')
+
         transactions = []
-        for tid, t in self.perform_request('TransHistory', {'count': limit}).iteritems():
-            if t['type'] == 1:
-                # Assume no fees for deopsit
-                transactions.append(Deposit(tid,
-                                            self._format_timestamp(t['timestamp']),
-                                            t['currency'],
-                                            t['amount'],
-                                            '',
-                                            0
-                                    ))
-            elif t['type'] == 2:
-                idx = t['desc'].find('address ')
-                if idx:
-                    address = t['desc'][idx+8:]
-                else:
-                    address = ''
-                # Withdraw fees are not provided by BTC-e API
-                transactions.append(Withdrawal(tid,
-                                            self._format_timestamp(t['timestamp']),
-                                            t['currency'],
-                                            t['amount'],
-                                            address
-                                    ))
-        return transactions
+        trades = []
+        for tid, t in self.perform_request('TransHistory', {'count': 9999999999999}).iteritems():
+            if parse_transactions and t['type'] in (1, 2):
+                if t['type'] == 1:
+                    # Assume no fees for deopsit
+                    transactions.append(Deposit(tid,
+                                                self._format_timestamp(t['timestamp']),
+                                                t['currency'],
+                                                t['amount'],
+                                                '',
+                                                0
+                                        ))
+                elif t['type'] == 2:
+                    idx = t['desc'].find('address ')
+                    if idx:
+                        address = t['desc'][idx+8:]
+                    else:
+                        address = ''
+                    # Withdraw fees are not provided by BTC-e API
+                    transactions.append(Withdrawal(tid,
+                                                self._format_timestamp(t['timestamp']),
+                                                t['currency'],
+                                                t['amount'],
+                                                address
+                                        ))
+            elif parse_trades and t['type'] in (4, 5):
+                m = self.re_buy.search(t['desc'])
+                if m:
+                    # This is a buy transaction
+                    amount = Decimal(m.groupdict().get('amount'))
+                    trades.append(Buy(
+                                        trade_id = get_trade_id(t, m),
+                                        base_currency = m.groupdict().get('base_currency'),
+                                        counter_currency = m.groupdict().get('counter_currency'),
+                                        datetime = self._format_timestamp(t['timestamp']),
+                                        order_id = int(m.groupdict().get('order_id')),
+                                        amount = amount,
+                                        price = Decimal(m.groupdict().get('price')),
+                                        fee = common.quantize(Decimal(m.groupdict().get('fee_percent')) * amount / 100),
+                                        fee_currency = m.groupdict().get('base_currency'),
+                                        )
+                                    )
+                    continue
+
+                m = self.re_sell.search(t['desc'])
+                if m:
+                    # This is a sell transation
+                    amount = Decimal(m.groupdict().get('amount'))
+                    price = Decimal(m.groupdict().get('price'))
+                    trades.append(Sell(
+                                        trade_id = get_trade_id(t, m),
+                                        base_currency = m.groupdict().get('base_currency'),
+                                        counter_currency = m.groupdict().get('counter_currency'),
+                                        datetime = self._format_timestamp(t['timestamp']),
+                                        order_id = int(m.groupdict().get('order_id')),
+                                        amount = amount,
+                                        price = price,
+                                        fee = common.quantize(Decimal(m.groupdict().get('fee_percent')) * (amount * price) / 100),
+                                        fee_currency = m.groupdict().get('counter_currency'),
+                                        )
+                                    )
+                    continue
+        return (transactions, trades)
+
+    def get_my_transactions(self, limit=None):
+        '''
+        :param limit: Not used because "TransHistory" method conrains and coints cancelations etc. as transaction
+        '''
+        return self._parse_transaction_history(parse_trades=False)[0]
 
     def get_my_funds(self):
         funds = {}
